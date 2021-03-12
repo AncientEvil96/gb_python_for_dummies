d632958fdfbe11a64cf7ce2b1947a3da96c11a10
Ilya Efimov <ilyaeg92@gmail.com>
2021-03-12 19:24:41 +0300

    обновил 6 задание

--- lesson_3/task_6_7.py
+++ lesson_3/task_6_7.py
@@ -6,3 +6,16 @@
 # Каждое слово состоит из латинских букв в нижнем регистре.
 # Нужно сделать вывод исходной строки, но каждое слово должно начинаться с заглавной буквы.
 # Используйте написанную ранее функцию int_func().
+
+def int_func(text_):
+    text_.lower()
+    for i in text_:
+        if i not in 'abcdefghijklmnopqrstuvwxyz ':
+            print('error: you are using non-latin letters')
+            return
+
+    print(f'Answer_1: {text_.capitalize()}')
+    print(f'Answer_2: {text_.title()}')
+
+
+int_func(input("enter latin letters text: "))
