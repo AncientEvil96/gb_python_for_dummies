13d06f265e09203bbfe18bdd750ec277263dd0f1
AncientEvil96 <bersek123@gmail.com>
2021-03-12 16:57:32 +0300

    loading first 5 task

--- lesson_3/task_4.py
+++ lesson_3/task_4.py
@@ -5,3 +5,27 @@
 # Подсказка: попробуйте решить задачу двумя способами.
 # Первый — возведение в степень с помощью оператора **.
 # Второй — более сложная реализация без оператора **, предусматривающая использование цикла.
+
+def my_func_1(x, y):
+    return x ** y
+
+
+def my_func_2(x, y):
+    result = 1
+    for i in range(abs(y)):
+        result *= x
+    if y >= 0:
+        return result
+    else:
+        return 1 / result
+
+
+x = abs(float(input("enter the number x: ")))
+
+while True:
+    y = int(input("enter the number -y: "))
+    if y < 0:
+        break
+
+print(round(my_func_1(x, y), 5))
+print(round(my_func_2(x, y), 5))
--- lesson_3/task_5.py
+++ lesson_3/task_5.py
@@ -5,3 +5,18 @@
 # Но если вместо числа вводится специальный символ, выполнение программы завершается.
 # Если специальный символ введён после нескольких чисел,
 # то вначале нужно добавить сумму этих чисел к полученной ранее сумме и после этого завершить программу.
+
+
+user_list = []
+
+while True:
+    user_list.extend(input('enter number across " ", if exit enter "q": ').split())
+    ext_q = user_list.count('q')
+    if ext_q>0:
+        index_q = user_list.index('q')
+        user_list = user_list[:index_q]
+        break
+    else:
+        print(sum(user_list))
+
+print(sum(user_list))
