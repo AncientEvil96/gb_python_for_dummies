56ba17e26dfb41591e71fbed551724e034e7710c
Ilya Efimov <ilyaeg92@gmail.com>
2021-03-19 00:29:34 +0300

    update 1,2

--- lesson_5/task_2.py
+++ lesson_5/task_2.py
@@ -1,2 +1,9 @@
 # Создать текстовый файл (не программно),
 # сохранить в нём несколько строк, выполнить подсчёт строк и слов в каждой строке.
+
+with open('test_1.txt', 'r+') as my_f:
+    my_f.write(f'\n test test \n test test \n test test')
+    my_f.seek(0)
+    text_f = my_f.read()
+    print(len(text_f.split()))
+    print(len(text_f.split('\n')))
