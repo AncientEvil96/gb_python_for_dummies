dd420cc0edd410db3ac0c0ecc8ab22a8ae330eb8
AncientEvil96 <bersek123@gmail.com>
2021-03-19 16:58:23 +0300

    update 1-6

--- lesson_5/task_2.py
+++ lesson_5/task_2.py
@@ -1,9 +1,17 @@
 # Создать текстовый файл (не программно),
 # сохранить в нём несколько строк, выполнить подсчёт строк и слов в каждой строке.
 
-with open('test_1.txt', 'r+') as my_f:
-    my_f.write(f'\n test test \n test test \n test test')
-    my_f.seek(0)
-    text_f = my_f.read()
-    print(len(text_f.split()))
-    print(len(text_f.split('\n')))
+from os import path
+
+my_f_name = 'test_1.txt'
+
+if path.exists(my_f_name):
+    with open(my_f_name, 'a+') as my_f:
+        my_f.write(f'\n test test \n test test \n test test')
+        my_f.seek(0)
+        text_f = my_f.read()
+        print(f'Words: {len(text_f.split())}')
+        lines = len(text_f.split('\n'))
+        print(f"Lines: {lines}")
+else:
+    print('not file')
