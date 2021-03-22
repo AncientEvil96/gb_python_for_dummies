aa9fb999d67dccab303625b5455e9de4e4b21b10
Ilya Efimov <ilyaeg92@gmail.com>
2021-03-22 22:07:29 +0300

    update 5

--- lesson_6/task_5.py
+++ lesson_6/task_5.py
@@ -6,3 +6,33 @@
 # Для каждого из классов методы должен выводить уникальное сообщение.
 # Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
 
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print('Запуск отрисовки.')
+
+
+class Pen(Stationery):
+
+    def draw(self):
+        print('Запуск Pen.')
+
+
+class Pencil(Stationery):
+
+    def draw(self):
+        print('Запуск Pencil.')
+
+
+class Handle(Stationery):
+
+    def draw(self):
+        print('Запуск Handle.')
+
+
+if __name__ == '__main__':
+    Pen.draw('Pen')
+    Pencil.draw('Pencil')
+    Handle.draw('Handle')
