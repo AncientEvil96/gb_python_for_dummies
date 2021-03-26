3f6ee02c2ababdc8da3655e38444e605a474ac60
AncientEvil96 <bersek123@gmail.com>
2021-03-26 16:44:13 +0300

    update 1

--- lesson_7/task_1.py
+++ lesson_7/task_1.py
@@ -7,3 +7,34 @@
 # Результатом сложения должна быть новая матрица.
 # Подсказка: сложение элементов матриц выполнять поэлементно — первый элемент первой строки первой матрицы
 # складываем с первым элементом первой строки второй матрицы и т.д.
+
+class Matrix:
+    def __init__(self, matrix_lines):
+        matrix_lines = matrix_lines
+        # найдем макс длинну матрицы
+        len_lines = [len(i) for i in matrix_lines]
+        self.max_m_len = max(len_lines)
+        # выровним матрицу если пришло неровное количество элементов
+        for line in matrix_lines:
+            if len(line) != self.max_m_len:
+                add_count_elem = self.max_m_len - len(line)
+                while add_count_elem > 0:
+                    add_count_elem -= 1
+                    line.append(0)
+
+        self.matrix = matrix_lines
+
+        # if len(line_m) == 1:
+        #     self.matrix =
+        #     self.line_m = line_m
+        #     self.len_mat = len(line_m)
+        # else
+        #     return 'error elements matrix'
+
+    # def __add__(self, other):
+    #     pass
+
+
+if __name__ == '__main__':
+    matrix1 = Matrix([[1, 2, 5], [3, 4]])
+    print(matrix1.matrix, '\n')
