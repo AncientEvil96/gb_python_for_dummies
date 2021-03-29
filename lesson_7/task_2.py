5969d3d3b0ddb831b187cb9011738decf199a426
AncientEvil96 <bersek123@gmail.com>
2021-03-29 09:20:36 +0300

    update 2

--- lesson_7/task_2.py
+++ lesson_7/task_2.py
@@ -7,3 +7,35 @@
 # Реализовать общий подсчет расхода ткани.
 # Проверить на практике полученные на этом уроке знания:
 # реализовать абстрактные классы для основных классов проекта, проверить на практике работу декоратора @property
+
+from abc import ABC, abstractmethod
+
+
+class Clothes(ABC):
+    def __init__(self, data_):
+        self.data_ = data_
+
+    @abstractmethod
+    def fabric_calculation(self, ):
+        pass
+
+
+class Coat(Clothes):
+
+    @property
+    def fabric_calculation(self):
+        return self.data_ / 6.5 + 0.5
+
+
+class Costume(Clothes):
+
+    @property
+    def fabric_calculation(self):
+        return 2 * self.data_ + 0.3
+
+
+if __name__ == '__main__':
+    coat = Coat(10)
+    costume = Costume(20)
+    print(coat.fabric_calculation)
+    print(costume.fabric_calculation)
